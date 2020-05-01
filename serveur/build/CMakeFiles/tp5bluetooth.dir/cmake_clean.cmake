af8a5d685f49c28b344f0fcf306430f363d20fce
setr <setr@localhost.localdomain>
2020-05-01 15:15:31 -0400

    Aider pour path

---
+++ serveur/build/CMakeFiles/tp5bluetooth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp5bluetooth.dir/Bluetooth_Server.c.o"
+  "tp5bluetooth.pdb"
+  "tp5bluetooth"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tp5bluetooth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ serveur/syncAndStartGDB.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Sync executable
+bn=$(basename $1)
+rsync -az $1/build/tp5bluetooth pi@$2:/home/pi/projects/projects/pi-bluetooth/tp5bluetooth
+#laboratoire2/tp2serveur
+
+# Execute GDB
+ssh pi@$2 "nohup gdbserver :4567 /home/pi/projects/pi-bluetooth/tp5bluetooth > /dev/null 2> /dev/null < /dev/null &"
+sleep 1
